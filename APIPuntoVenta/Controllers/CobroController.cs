b70cbdc146d8800c313e1bb3ccad18d0b7be7c81
Nahuel Guerrero <guerreronahuel@gmail.com>
2021-07-26 23:59:56 -0300

    Add project files.

---
+++ APIPuntoVenta/Controllers/CobroController.cs
@@ -0,0 +1,36 @@
+﻿using APIPuntoVenta.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPuntoVenta.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CobroController : ControllerBase
+	{
+		[HttpGet]
+		public string Get()
+		{
+			return "Llegue";
+		}
+
+		[HttpPost("CalcularCambio")]
+		public ActionResult<Cambio> CalcularCambio(decimal totalAPagar, decimal totalPagado)
+		{
+			if (totalAPagar <= 0) {
+				return BadRequest("El total a Pagar no puede ser igual o menor a 0");
+			}
+
+			if (totalPagado < totalAPagar) {
+				return BadRequest("El total pagado es menor al importe a pagar. Solicite al cliente completar el monto con el dinero faltante.");
+			}
+
+			Cambio cambio = new Cambio();
+
+			return Ok(cambio);
+		}
+	}
+}
---
+++ APIPuntoVenta/Models/Cambio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPuntoVenta.Models
+{
+	public class Cambio
+	{
+		public int BRL100 { get; set; }
+		public int BRL50 { get; set; }
+		public int BRL20 { get; set; }
+		public int BRL10 { get; set; }
+		public int R050 { get; set; }
+		public int R010 { get; set; }
+		public int R005 { get; set; }
+		public int R001 { get; set; }
+
+		public string message
+		{
+			get
+			{
+				string message = "Entregar ";
+				return message;
+			}
+		}
+	}
+}
