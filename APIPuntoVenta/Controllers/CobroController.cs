099e0efd219d31f65a83d083bbdc8e2d0752b487
Nahuel Guerrero <guerreronahuel@gmail.com>
2021-07-27 21:38:31 -0300

    Se simplifica resta

--- APIPuntoVenta/Controllers/CobroController.cs
+++ APIPuntoVenta/Controllers/CobroController.cs
@@ -37,49 +37,49 @@ namespace APIPuntoVenta.Controllers
 			if (restante >= 100)
 			{
 				cambio.BRL100 = (int)restante / 100;
-				restante = restante - (cambio.BRL100 * 100);
+				restante -= (cambio.BRL100 * 100);
 			}
 
 			if (restante >= 50)
 			{
 				cambio.BRL50 = (int)restante / 50;
-				restante = restante - (cambio.BRL50 * 50);
+				restante -= (cambio.BRL50 * 50);
 			}
 
 			if (restante >= 20)
 			{
 				cambio.BRL20 = (int)restante / 20;
-				restante = restante - (cambio.BRL20 * 20);
+				restante -= (cambio.BRL20 * 20);
 			}
 
 			if (restante >= 10)
 			{
 				cambio.BRL10 = (int)restante / 10;
-				restante = restante - (cambio.BRL10 * 10);
+				restante -= (cambio.BRL10 * 10);
 			}
 
 			if (restante >= (decimal)0.50)
 			{
 				cambio.R050 = (int) (restante / (decimal)0.50);
-				restante = restante - (decimal)(cambio.R050 * 0.50);
+				restante -= (decimal)(cambio.R050 * 0.50);
 			}
 
 			if (restante >= (decimal)0.10)
 			{
 				cambio.R010 = (int)(restante / (decimal)0.10);
-				restante = restante - (decimal)(cambio.R010 * 0.10);
+				restante -= (decimal)(cambio.R010 * 0.10);
 			}
 
 			if (restante >= (decimal)0.05)
 			{
 				cambio.R005 = (int)(restante / (decimal)0.05);
-				restante = restante - (decimal)(cambio.R005 * 0.05);
+				restante -= (decimal)(cambio.R005 * 0.05);
 			}
 
 			if (restante >= (decimal)0.01)
 			{
 				cambio.R001 = (int)(restante / (decimal)0.01);
-				restante = restante - (decimal)(cambio.R001 * 0.01);
+				restante -= (decimal)(cambio.R001 * 0.01);
 			}
 
 			return Ok(cambio);
